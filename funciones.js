16f693c50d1057915ea5920b2dce71f98f6986a8
Totoaguil20 <totoaguil022@gmail.com>
2023-06-07 12:46:07 -0300

    galeria 3.0

--- funciones.js
+++ funciones.js
@@ -1,7 +1,8 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-   function mostrarAlerta() {
+   
+  function mostrarAlerta() {
     alert("Hola, esto es una alerta de JavaScript");
   }
   
@@ -23,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
   
+
   function validateEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/;
     return regex.test(email);
@@ -40,4 +42,63 @@ document.addEventListener("DOMContentLoaded", function() {
       document.querySelector(".nav-links")
         .classList.toggle("nav-links-responsive");
     });
-});
+
+
+
+
+
+
+    //Galeria de Fotos
+
+
+    const anterior = document.getElementById("anterior");
+    const siguiente = document.getElementById("siguente");
+    const imageSlider = document.querySelector(".imageSlider");
+    const galleryImages = document.querySelector(".gallery-images");
+    const switchButton = document.getElementById("switchButton");
+    const contenedorSlider = document.querySelector(".slider-container");
+    
+    const imagenes = [
+      'img/imagen1.jpg',
+      'img/imagen2.jpg',
+      'img/imagen3.jpg',
+      'img/imagen4.jpg'
+    ];
+    var currentImageIndex = 0;
+    var isGalleryView = false;
+   
+    
+    anterior.addEventListener("click", anteriorImagen);
+    siguiente.addEventListener("click", proximaImagen);
+    switchButton.addEventListener("click", toggleView);
+
+    function anteriorImagen() {
+      console.log("anterior");
+      currentImageIndex = (currentImageIndex - 1 + imagenes.length) % imagenes.length;
+      updateSlider();
+    }
+
+    function proximaImagen() {
+      console.log("siguiente");
+      currentImageIndex = (currentImageIndex + 1) % imagenes.length;
+      updateSlider();
+    }
+
+    function updateSlider() {
+      const currentImage = imagenes[currentImageIndex];
+      imageSlider.querySelector('img').src = currentImage;
+    }
+    function toggleView() {
+      if (isGalleryView) {
+        switchButton.textContent = "Ver vista galería de imágenes";
+        contenedorSlider.style.display = "block";
+        galleryImages.style.display = "none";
+      } else {
+        switchButton.textContent = "Ver vista deslizador de imágenes";
+        contenedorSlider.style.display = "none";
+        galleryImages.style.display = "block";
+      }
+
+      isGalleryView = !isGalleryView;
+    }
+  });
